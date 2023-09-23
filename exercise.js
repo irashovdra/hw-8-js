d79d2ba4717f4efe620733dd831ca85e5a2ac016
irashovdra <109846925+irashovdra@users.noreply.github.com>
2023-09-22 20:43:19 -0500

    html & js

---
+++ exercise.js
@@ -0,0 +1,17 @@
+const b = 10;
+
+if (true) {
+  const a = 5;
+  console.log(a);
+  console.log(b);
+}
+console.log(a);
+
+if (true) {
+  const b = 10;
+
+  console.log(a);
+  console.log(b);
+}
+
+console.log(b);
